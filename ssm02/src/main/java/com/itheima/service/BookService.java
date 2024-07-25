8314e4deff644b8bb556208a9137b19191e2d5e7
WinnieWei0 <13250578375@163.com>
2024-07-25 21:56:56 +0800

    添加事务

--- ssm02/src/main/java/com/itheima/service/BookService.java
+++ ssm02/src/main/java/com/itheima/service/BookService.java
@@ -4,9 +4,11 @@ import com.itheima.domain.Book;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional // 添加事务
 public interface BookService {
     public Boolean save(Book book);
 
